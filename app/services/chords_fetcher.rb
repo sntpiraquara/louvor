6cd260ef55258ce1b2b004df530165c852535e54
Neri J. Jakubowski Jr <neri@nerijunior.com>
2022-07-12 00:09:24 -0300

    better chords finder

--- app/services/chords_fetcher.rb
+++ app/services/chords_fetcher.rb
@@ -9,7 +9,14 @@ class ChordsFetcher
   def run
     artist_slug = first_artist_match
 
-    @chords = fetch_chords(artist_slug)
+    chords = fetch_chords(artist_slug, @title.parameterize)
+
+    unless chords
+      song_slug = first_song_match(artist_slug)
+      chords = fetch_chords(artist_slug, song_slug)
+    end
+
+    @chords = chords
   end
 
   private
@@ -31,15 +38,35 @@ class ChordsFetcher
     json['response']['docs'][0]['d']
   end
 
-  def fetch_chords(slug)
-    song_title = @title.parameterize
+  def fetch_chords(artist_slug, song_slug)
     conn = Faraday.new(
       url: 'https://www.cifraclub.com.br/'
     )
 
-    response = conn.get("#{slug}/#{song_title}/imprimir.html")
+    response = conn.get("#{artist_slug}/#{song_slug}/imprimir.html")
+
+    return false if response.status != 200
 
     parser = Nokogiri::HTML.parse(response.body)
     parser.search('pre').text
+  end
+
+  def first_song_match(artist_slug)
+    song_title = @title.parameterize
+
+    conn = Faraday.new(
+      url: 'https://studiosolsolr-a.akamaihd.net',
+      params: {
+        q: "#{artist_slug} #{song_title}"
+      }
+    )
+
+    response = conn.get('/cc/h2/')
+
+    return false if response.status != 200
+
+    json = JSON.parse(response.body[1...-2])
+
+    json['response']['docs'][0]['u']
   end
 end
